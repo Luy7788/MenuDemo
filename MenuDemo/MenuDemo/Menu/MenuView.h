f29c29d0d6d3fe7afef8863d2f8e5fda34ab9e0d
Lying <lying_555@163.com>
2016-06-28 16:48:08 +0800

    no message

--- MenuDemo/MenuDemo/Menu/MenuView.h
+++ MenuDemo/MenuDemo/Menu/MenuView.h
@@ -14,12 +14,28 @@
  
 +(instancetype)MenuViewWithDependencyView:(UIView *)dependencyView MenuView:(UIView *)leftmenuView isShowCoverView:(BOOL)isCover;
 
+/**
+ *  初始化方法
+ *
+ *  @param dependencyView 传入需要滑出菜单的控制器的view
+ *  @param leftmenuView   传入需要显示的菜单的view
+ *  @param isCover        bool值，是否有右边遮挡的阴影
+ *
+ *  @return self
+ */
 -(instancetype)initWithDependencyView:(UIView *)dependencyView MenuView:(UIView *)leftmenuView isShowCoverView:(BOOL)isCover;
     
-
+/**
+ *  展开菜单，可放进点击事件内
+ */
 -(void)show;
-
+/**
+ *  关闭菜单不带动画效果
+ */
 -(void)hidenWithoutAnimation;
+/**
+ *  关闭菜单带动画效果
+ */
 -(void)hidenWithAnimation;
 
 @end
